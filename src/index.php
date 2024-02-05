90a66f38a5c9d3299f7f73be70cbcc29dfe2fef8
Ubuntu <ubuntu@ip-172-31-19-192.ec2.internal>
2024-02-05 14:50:56 +0000

    He subido los 3 archivos

---
+++ src/index.php
@@ -0,0 +1,29 @@
+/*<?php*/
+/*
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/*
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+/*define( 'WP_USE_THEMES', true );*/
+
+/* Loads the WordPress Environment and Template */
+/*require __DIR__ . '/wp-blog-header.php';*/
+
+
+
+<html>
+<head>
+	<title>Actividad 1.3.1</title>
+</head>
+<body>
+	<?php echo "<h1>Actividad 1.3.1: LAMP Sever. Autor: Javier Ortiz</h1>"; ?>
+</body>
+</html>
+
